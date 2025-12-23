be4442cca01271dcd4f8bbd3297b483c0d066e83
josiaschweizer <josia.schweizer@gmail.com>
2025-12-23 16:29:16 +0100

    WIP: trying to run verno

--- server/src/main/java/ch/verno/server/Application.java
+++ server/src/main/java/ch/verno/server/Application.java
@@ -5,8 +5,12 @@ import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "ch.verno")
+@EnableJpaRepositories(basePackages = "ch.verno.db.jpa")
+@EntityScan(basePackages = "ch.verno.db.entity")
 @StyleSheet(Lumo.STYLESHEET)
 @StyleSheet(Lumo.UTILITY_STYLESHEET)
 @StyleSheet("styles.css")
