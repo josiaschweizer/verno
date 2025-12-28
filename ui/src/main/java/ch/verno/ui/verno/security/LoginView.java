ccf87842fcc1994f7a98bd4a2b70760853ab9b5e
josiaschweizer <josia.schweizer@gmail.com>
2025-12-28 19:42:33 +0100

    feat: set sidebar closed on login page

--- ui/src/main/java/ch/verno/ui/verno/security/LoginView.java
+++ ui/src/main/java/ch/verno/ui/verno/security/LoginView.java
@@ -1,6 +1,9 @@
 package ch.verno.ui.verno.security;
 
+import ch.verno.ui.base.MainLayout;
 import ch.verno.ui.lib.Routes;
+import com.vaadin.flow.component.AttachEvent;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -23,5 +26,17 @@ public class LoginView extends VerticalLayout {
     login.getStyle().set("max-width", "360px");
 
     add(login);
+  }
+
+  @Override
+  protected void onAttach(final AttachEvent attachEvent) {
+    super.onAttach(attachEvent);
+
+    UI.getCurrent()
+            .getChildren()
+            .filter(c -> c instanceof MainLayout)
+            .map(c -> (MainLayout) c)
+            .findFirst()
+            .ifPresent(mainLayout -> mainLayout.setDrawerOpened(false));
   }
 }
