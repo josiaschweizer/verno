790aa491fa931290e9afe9edaba49ced8fa1b488
josiaschweizer <josia.schweizer@gmail.com>
2026-01-13 08:53:09 +0100

    feat: verno-36 Participant Grid - Export all Participants
    - participant list report

--- report/src/main/java/ch/verno/report/participant/ParticipantReport.java
+++ report/src/main/java/ch/verno/report/participant/ParticipantReport.java
@@ -1,9 +1,14 @@
 package ch.verno.report.participant;
 
+import ch.verno.common.exceptions.report.PDFRendererException;
 import ch.verno.report.base.BaseReport;
 import ch.verno.report.dto.ParticipantListReportDto;
+import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
 import jakarta.annotation.Nonnull;
 import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.io.ByteArrayOutputStream;
 
 public class ParticipantReport extends BaseReport<ParticipantListReportDto> {
 
@@ -12,8 +17,22 @@ public class ParticipantReport extends BaseReport<ParticipantListReportDto> {
   }
 
   @Override
-  public byte[] generateReportPdf(@Nonnull final ParticipantListReportDto reportData) {
-    return new byte[0];
+  public byte[] generateReportPdf(@Nonnull final ParticipantListReportDto dto) {
+    final var context = new Context();
+    context.setVariable("participants", dto.participantList());
+
+    final var html = templateEngine.process(getTemplate(), context);
+
+    try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+      final var pdfBuilder = new PdfRendererBuilder();
+      pdfBuilder.useFastMode();
+      pdfBuilder.withHtmlContent(html, null);
+      pdfBuilder.toStream(out);
+      pdfBuilder.run();
+      return out.toByteArray();
+    } catch (Exception e) {
+      throw new PDFRendererException("Failed to render participant list PDF", e);
+    }
   }
 
   @Nonnull
