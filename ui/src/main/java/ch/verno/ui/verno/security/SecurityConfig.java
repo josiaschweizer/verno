520d2f3f35088b388c5dab9773a3a68d8525a086
josiaschweizer <josia.schweizer@gmail.com>
2026-02-06 14:41:11 +0100

    feat: verno-71
    - cleanup security config

--- ui/src/main/java/ch/verno/ui/verno/security/SecurityConfig.java
+++ ui/src/main/java/ch/verno/ui/verno/security/SecurityConfig.java
@@ -5,13 +5,12 @@ import com.vaadin.flow.spring.security.VaadinSecurityConfigurer;
 import jakarta.annotation.Nonnull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Profile("prod")
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -19,27 +18,20 @@ public class SecurityConfig {
   @Bean
   @Nonnull
   SecurityFilterChain securityFilterChain(@Nonnull HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests(auth -> auth
-            .requestMatchers(ApiUrl.TEMP_FILE_REPORT + "/**").permitAll()
-            .requestMatchers(ApiUrl.TEMP_FILE_IMPORT + "/**").permitAll()
-            .requestMatchers(ApiUrl.TEMP_FILE_EXPORT + "/**").permitAll()
-            .requestMatchers("/api/v1/tenants").permitAll()
-    );
-
-    http = http.with(VaadinSecurityConfigurer.vaadin(), configurer -> {
-      configurer.loginView(LoginView.class);
-    });
-
-    http.csrf(csrf -> csrf
-            .ignoringRequestMatchers(ApiUrl.TEMP_FILE_REPORT + "/**")
-            .ignoringRequestMatchers(ApiUrl.TEMP_FILE_IMPORT + "/**")
-            .ignoringRequestMatchers(ApiUrl.TEMP_FILE_EXPORT + "/**")
-            .ignoringRequestMatchers("/api/v1/tenants")
-    );
-
-    http.headers(headers -> headers
-            .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
-    );
+    http
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers(ApiUrl.TEMP_FILE_REPORT + "/**").permitAll()
+                    .requestMatchers(ApiUrl.TEMP_FILE_IMPORT + "/**").permitAll()
+                    .requestMatchers(ApiUrl.TEMP_FILE_EXPORT + "/**").permitAll()
+                    .requestMatchers("/api/v1/tenants").permitAll()
+            )
+            .with(VaadinSecurityConfigurer.vaadin(), configurer -> {
+              configurer.loginView(LoginView.class);
+            })
+            .csrf(AbstractHttpConfigurer::disable)
+            .headers(headers -> headers
+                    .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+            );
 
     return http.build();
   }
