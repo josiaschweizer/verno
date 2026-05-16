b29f75b4a66ac60d2ac458ea085063d108869103
josiaschweizer <josia.schweizer@gmail.com>
2026-05-16 10:07:12 +0200

    feat: verno-150
    - create new va inline notification

--- publ/src/main/java/ch/verno/publ/CssImportConstants.java
+++ publ/src/main/java/ch/verno/publ/CssImportConstants.java
@@ -1,9 +1,16 @@
 package ch.verno.publ;
 
+import org.jetbrains.annotations.NonNls;
+
 public class CssImportConstants {
 
-  public static final String VA_BUTTON = "./components/button/va-button.css";
+  // components
+  @NonNls public static final String VA_BUTTON = "./components/button/va-button.css";
+  @NonNls public static final String ACCORDION_WIDGET_BASE = "./components/widget/va-accordion-widget-base.css";
 
-  public static final String ACCORDION_WIDGET_BASE = "./components/widget/va-accordion-widget-base.css";
+  @NonNls public static final String VA_NOTIFICATION = "./components/notification/va-notification.css";
+  @NonNls public static final String VA_INLINE_NOTIFICATION = "./components/notification/inline/va-inline-notification.css";
+
+  // apps
 
 }
